8444a26049a644273995780a804518735cf80ad5
mgsaglam <muhammetfeyzijobs@gmail.com>
2020-10-07 23:30:55 +0300

    project validations

--- linacreative/src/main/java/com/projectmanagement/domain/Project.java
+++ linacreative/src/main/java/com/projectmanagement/domain/Project.java
@@ -1,35 +1,46 @@
 package com.projectmanagement.domain;
 
 import lombok.Data;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
+
 
 @Entity
 @Data
 public class Project {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message ="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
-
-    private Date created_at;
-    private Date updated_at;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
 
     @PrePersist
     protected  void onCreate(){
-        this.created_at= new Date();
+        this.created_At= new Date();
     }
 
     @PreUpdate
     protected  void onUpdate(){
-        this.updated_at = new Date();
+        this.updated_At = new Date();
     }
 }
