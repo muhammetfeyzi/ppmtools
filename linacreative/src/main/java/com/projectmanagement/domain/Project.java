99149e844829c70b48090d14c127fa2307844a2e
mgsaglam <muhammetfeyzijobs@gmail.com>
2020-10-07 22:06:52 +0300

    Project object and Project repository

--- linacreative/src/main/java/com/projectmanagement/domain/Project.java
+++ linacreative/src/main/java/com/projectmanagement/domain/Project.java
@@ -1,4 +1,35 @@
 package com.projectmanagement.domain;
 
+import lombok.Data;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+
+    private Date created_at;
+    private Date updated_at;
+
+    @PrePersist
+    protected  void onCreate(){
+        this.created_at= new Date();
+    }
+
+    @PreUpdate
+    protected  void onUpdate(){
+        this.updated_at = new Date();
+    }
 }
